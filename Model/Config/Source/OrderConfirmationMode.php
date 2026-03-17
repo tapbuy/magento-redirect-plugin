19faece03197c3e135ba15a7634127b870b9a8f1
Valentin LÉVÊQUE <valentin.leveque@tapbuy.io>
2026-03-17 13:20:38 +0100

    PDT-480 Missing docstring
    PDT-480

--- Model/Config/Source/OrderConfirmationMode.php
+++ Model/Config/Source/OrderConfirmationMode.php
@@ -1,6 +1,19 @@
 <?php
 
 declare(strict_types=1);
+
+/**
+ * Source model for the "Order Confirmation Mode" admin config option.
+ *
+ * Provides the list of available strategies used to capture the order
+ * confirmation event and forward it to the Tapbuy pixel:
+ *  - GraphQL Mutation: triggered via the checkout GraphQL mutation
+ *  - OrderSaveAfter Observer: triggered by the sales_order_save_after event
+ *  - Both: triggers both strategies simultaneously
+ *
+ * @category  Tapbuy
+ * @package   Tapbuy_RedirectTracking
+ */
 namespace Tapbuy\RedirectTracking\Model\Config\Source;
 
 use Magento\Framework\Data\OptionSourceInterface;
@@ -8,6 +21,11 @@ use Tapbuy\RedirectTracking\Api\ConfigInterface;
 
 class OrderConfirmationMode implements OptionSourceInterface
 {
+    /**
+     * Return option array for use in admin system config select fields.
+     *
+     * @return array
+     */
     public function toOptionArray()
     {
         return [
